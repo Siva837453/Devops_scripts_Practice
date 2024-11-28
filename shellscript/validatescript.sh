6f2e50f9648a807c1ef4f39d092644605e2e71f7
Siva837453 <sivaallaka777@gmail.com>
2024-11-28 21:24:42 +0530

    Script

--- shellscript/validatescript.sh
+++ shellscript/validatescript.sh
@@ -3,15 +3,17 @@ DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$DATE.log
 
-
+R="/e[31m"
+G="/e[32m"
+N="/e[0m"
 
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  "$2...failure"
+        echo -e "$2...$R failure $N"
     else    
-        echo "$2...success"
+        echo "$2...$G success $N"
     fi
 }
 USERID=$(id -u)
